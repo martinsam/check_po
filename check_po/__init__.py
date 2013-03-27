ce32c24956183e50342dfb308a5d983e2c8ef87a
Samuel Martin <martin.sam@gmail.com>
2013-03-28 00:38:55 +0100

    Update __init__.py

--- check_po/__init__.py
+++ check_po/__init__.py
@@ -17,24 +17,24 @@ def main():
     po = polib.pofile(po_file_path)
 
     UNTRANSLATED = 0
-    for entry in po.untranslated_entries():
-        UNTRANSLATED += 1
-        print "UNTRANSLATED\t", entry.msgid.encode('utf-8')
+    #for entry in po.untranslated_entries():
+    #    UNTRANSLATED += 1
+    #    print "UNTRANSLATED\t", entry.msgid.encode('utf-8')
 
     FUZZY = 0
     for entry in po.fuzzy_entries():
         FUZZY += 1
         print "FUZZY\t", entry.msgid.encode('utf-8'), entry.msgstr.encode('utf-8')
 
-    if FUZZY or UNTRANSLATED:
-        print '\n----------'
-        if FUZZY:
-            print "%d FUZZY string found." % FUZZY
-        if UNTRANSLATED:
-            print "%d UNTRANSLATED string found." % UNTRANSLATED
-        print '----------'
-        print "Edit: %s\n\n" % os.path.abspath(po_file_path)
-        sys.exit(100)
+    #if FUZZY or UNTRANSLATED:
+     #    print '\n----------'
+     #   if FUZZY:
+    #        print "%d FUZZY string found." % FUZZY
+    #    if UNTRANSLATED:
+     #       print "%d UNTRANSLATED string found." % UNTRANSLATED
+     #   print '----------'
+     #   print "Edit: %s\n\n" % os.path.abspath(po_file_path)
+     #   sys.exit(100)
 
     print "%s: PO File OK\n\n" % po_file_path
     sys.exit(0)
